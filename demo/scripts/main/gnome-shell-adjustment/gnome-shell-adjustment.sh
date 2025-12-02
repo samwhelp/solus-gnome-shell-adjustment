88658c337e8e3e9690c1d9bb10abdade113dd0fe
samwhelp <samwhelp@users.noreply.github.com>
2025-12-02 14:28:48 +0800

    fix

--- demo/scripts/main/gnome-shell-adjustment/gnome-shell-adjustment.sh
+++ demo/scripts/main/gnome-shell-adjustment/gnome-shell-adjustment.sh
@@ -848,6 +848,70 @@ mod_tool_gnome_text_editor_config () {
 
 
 ##
+## ## Tool / Gedit / Config
+##
+
+mod_tool_gedit_config () {
+
+
+	gsettings set org.gnome.gedit.preferences.editor auto-indent true
+
+	gsettings set org.gnome.gedit.preferences.editor auto-save false
+
+	gsettings set org.gnome.gedit.preferences.editor auto-save-interval 10
+
+	gsettings set org.gnome.gedit.preferences.editor background-pattern 'none'
+
+	gsettings set org.gnome.gedit.preferences.editor bracket-matching true
+
+	gsettings set org.gnome.gedit.preferences.editor create-backup-copy false
+
+	gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+
+	gsettings set org.gnome.gedit.preferences.editor display-right-margin true
+
+	gsettings set org.gnome.gedit.preferences.editor editor-font 'Monospace 14'
+
+	gsettings set org.gnome.gedit.preferences.editor ensure-trailing-newline true
+
+	gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
+
+	gsettings set org.gnome.gedit.preferences.editor insert-spaces false
+
+	gsettings set org.gnome.gedit.preferences.editor max-undo-actions 2000
+
+	gsettings set org.gnome.gedit.preferences.editor restore-cursor-position true
+
+	gsettings set org.gnome.gedit.preferences.editor right-margin-position 80
+
+	gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+
+	gsettings set org.gnome.gedit.preferences.editor search-highlighting true
+
+	gsettings set org.gnome.gedit.preferences.editor smart-home-end 'after'
+
+	gsettings set org.gnome.gedit.preferences.editor syntax-highlighting true
+
+	gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+
+	gsettings set org.gnome.gedit.preferences.editor use-default-font false
+
+	gsettings set org.gnome.gedit.preferences.editor wrap-last-split-mode 'word'
+
+	gsettings set org.gnome.gedit.preferences.editor wrap-mode 'word'
+
+
+	return 0
+}
+
+
+
+
+
+
+
+
+##
 ## ## Gnome Shell / Config
 ##
 
@@ -900,6 +964,8 @@ mod_tool_config () {
 	mod_tool_nautilus_config
 
 	mod_tool_gnome_text_editor_config
+
+	#mod_tool_gedit_config
 
 
 	return 0
